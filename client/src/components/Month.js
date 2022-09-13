ead97738b0d19790d08f01a186a70248e6f2ff18
JP <j.p.walter@outlook.com>
2022-09-13 17:52:43 -0400

    Now displays number of dreams per month when month is hovered

--- client/src/components/Month.js
+++ client/src/components/Month.js
@@ -3,17 +3,29 @@ import "./Month.css";
 
 export default function Month(props) {
   const [isFocus, setFocus] = React.useState(true);
+  const [display, setDisplay] = React.useState(props.month);
 
+  //Changes class when clicked, maybe change all classes so they fade?
   function handleClick() {
     setFocus(!isFocus);
   }
+  function handleMouseOver() {
+    setDisplay(0);
+  }
+  function handleMouseOut() {
+    setDisplay(props.month);
+  }
 
   return (
-    <div
-      className={isFocus ? "month-name" : "month-name test"}
-      onClick={handleClick}
-    >
-      {props.month}
+    <div>
+      <div
+        className={isFocus ? "month-name" : "month-name focus"}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        {display}
+      </div>
     </div>
   );
 }
