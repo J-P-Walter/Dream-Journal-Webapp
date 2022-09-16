ad11558141d7c754b1a5328ffc047bbe5dde39d8
JP <j.p.walter@outlook.com>
2022-09-16 14:34:47 -0400

    Now the client doesn't work either lol

--- server/server.js
+++ server/server.js
@@ -18,6 +18,6 @@ app.listen(PORT, () => {
   });
   console.log(`Server is running on port ${PORT}`);
 });
-app.get("/", (req, res) => {
-  res.send("Hello from Express!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello from Express!");
+// });
